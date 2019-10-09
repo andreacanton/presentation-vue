8689a6ba3a049f825d460975a8c1a8ad4b6469f0
Andrea Canton <andrea.canton@gmail.com>
2019-10-09 14:42:21 +0200

    Initial slide

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <SlideTemplate />
+</template>
+
+<script>
+import SlideTemplate from "./components/SlideTemplate";
+
+export default {
+  name: "app",
+  components: {
+    SlideTemplate
+  }
+};
+</script>
+
+<style lang="scss">
+* {
+}
+</style>
---
+++ src/components/SlideTemplate.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="slide">
+    <div class="content">
+      <h1>Slide title here</h1>
+      <p>This is a paragraph</p>
+      <ul>
+        <li>this is a list item</li>
+        <li>another one...</li>
+        <li>one more...</li>
+        <li>and finally this list item</li>
+      </ul>
+    </div>
+    <div class="image">
+      <img src="../assets/nyan.jpg" alt="nyan cat" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SlideTemplate"
+};
+</script>
+
+<style>
+.slide {
+  background: #77b9f1;
+  padding: 30px;
+  height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
